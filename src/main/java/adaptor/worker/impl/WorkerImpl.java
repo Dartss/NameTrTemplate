ec6c615c59b8e05e3f29d2331db659e848ec2669
Dmitry Bulaniy <dmitry.bulaniy@smarty.systems>
2016-11-04 12:29:31 +0200

    before test

--- src/main/java/adaptor/worker/impl/WorkerImpl.java
+++ src/main/java/adaptor/worker/impl/WorkerImpl.java
@@ -1,5 +1,13 @@
 package adaptor.worker.impl;
 
+import adaptor.impl.AdaptorImpl;
+import com.google.gson.JsonParser;
+import common.http.HttpRequestHandler;
+import common.http.HttpResponse;
+import common.template.manageradaptor.vo.JobVO;
+import lebedev.YandexKeyVO;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletionService;
@@ -7,12 +15,6 @@ import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
-
-import adaptor.impl.AdaptorImpl;
-import common.http.HttpRequestHandler;
-import common.http.HttpResponse;
-import common.template.manageradaptor.vo.JobVO;
-import lebedev.YandexKeyVO;
 
 /**
  * Worker should get job from adaptor as param in constructor
@@ -27,11 +29,13 @@ public class WorkerImpl implements Runnable
     private CompletionService<JobVO> completionService;
     private static final String yandexUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate?lang=ar&key=";
     private YandexKeyVO yandexKeyVO;
+    private JsonParser jsonParser;
 
     private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     public WorkerImpl(AdaptorImpl adaptor, JobVO jobVO)
     {
+	this.jsonParser = new JsonParser();
 	this.adaptor = adaptor;
 	this.jobVO = jobVO;
 	this.executorService = Executors.newFixedThreadPool(1);
@@ -54,7 +58,8 @@ public class WorkerImpl implements Runnable
 	    LOGGER.info("Worker makes call to Yandex: " + url);
 	    HttpResponse response = httpRequestHandler.executePost(url, null, params);
 
-	    translated = response.getJsonBody().getString("text");
+	    JSONObject sourceObject = response.getJsonBody();
+	    translated = sourceObject.getJSONArray("text").getJSONObject(0).toString();
 	    statusCode = response.getStatusCode();
 	    if (statusCode == 200) {
 	    	LOGGER.info("Word " + toTranslate + " translated to " + translated);
