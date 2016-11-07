9b8a3fc109fe1bd869101a985f027ddc4211e117
Dmitry Bulaniy <dmitry.bulaniy@smarty.systems>
2016-11-07 09:37:19 +0200

    upgrade

--- src/main/java/manager/impl/controller/QueueLoader.java
+++ src/main/java/manager/impl/controller/QueueLoader.java
@@ -3,6 +3,7 @@ package manager.impl.controller;
 import common.jdbc.JdbcHandler;
 import common.queuer.QueuerPoolHandler;
 import common.queuer.QueuerPoolHandlerImpl;
+import org.apache.http.io.SessionOutputBuffer;
 import org.apache.log4j.Logger;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Pipeline;
@@ -32,28 +33,46 @@ public class QueueLoader
 	this.queuerPoolHandler = new QueuerPoolHandlerImpl(host, port);
     }
 
+    /**
+     *
+     */
+
     public void pullFromSourceToQueue()
     {
-	try (BufferedReader in = new BufferedReader(new FileReader(FILE_PATH)))
+	try
 	{
+	    long startTime = System.currentTimeMillis();
 	    logger.info("Trying to load from file with path : " + FILE_PATH);
-	    String init;
-	    while ((init = in.readLine()) != null)
-	    {
-		List<String> splittedName = Arrays.asList(init.split(" +"));
-		for (String word : splittedName)
+	    List<Thread> sqlPushers = new ArrayList<>();
+
+	    pullFromFileToRedis();
+
+	    for (int i = 0; i < 50; i ++) {
+	        Thread pusher = new Thread(new Runnable()
 		{
-		    if((this.queuerPoolHandler.sadd(QUEUE_NAME, word) == 1)){
-			try
-			{
-			    jdbcHandler.insert(SQL_QUEUERY, Arrays.asList(word));
-			} catch (SQLException e)
-			{
-			    e.printStackTrace();
+		    @Override public void run()
+		    {
+		        String word;
+			while ((word = queuerPoolHandler.spop("names")) != null) {
+			    try
+			    {
+				jdbcHandler.insert(SQL_QUEUERY, Arrays.asList(word));
+			    } catch (SQLException e)
+			    {
+				e.printStackTrace();
+			    }
 			}
 		    }
-		}
+		});
+	        sqlPushers.add(pusher);
+		pusher.start();
 	    }
+	    for (Thread pusher : sqlPushers) {
+	        pusher.join();
+	    }
+
+
+	    System.out.println("---------------Time : " + (System.currentTimeMillis() - startTime));
 	} catch (FileNotFoundException e)
 	{
 	    try
@@ -78,6 +97,27 @@ public class QueueLoader
 	{
 	    logger.info("No sources found ------------!!!!!");
 	    e.printStackTrace();
+	} catch (InterruptedException e)
+	{
+	    e.printStackTrace();
 	}
+    }
+
+    private void pullFromFileToRedis() throws IOException
+    {
+        Pipeline pipeline = queuerPoolHandler.getJedisPipeline();
+	BufferedReader in = new BufferedReader(new FileReader(FILE_PATH));
+	String init;
+
+	while ((init = in.readLine()) != null)
+	{
+	    List<String> splittedName = Arrays.asList(init.split(" +"));
+	    for (String word : splittedName)
+	    {
+		pipeline.sadd(QUEUE_NAME, word);
+	    }
+	}
+
+	pipeline.sync();
     }
 }
